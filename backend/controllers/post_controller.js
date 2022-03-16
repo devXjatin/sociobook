bc478a29d0635fd6b5b7e0def11afc28ea68b279
devXjatin <jatin13280@gmail.com>
2022-03-16 16:27:13 +0530

    post updated route and controller

--- backend/controllers/post_controller.js
+++ backend/controllers/post_controller.js
@@ -1,142 +1,161 @@
-const Post = require('../model/post');
-const User = require('../model/user')
+const Post = require("../model/post");
+const User = require("../model/user");
 
-exports.createPost = async(req, res)=>{
-    try {
-        const newPostData ={
-            caption:req.body.caption,
-            image:{
-                //Todo
-                public_id:"req.body.public_id",
-                url:"req.body.url"
-            },
-            owner:req.user._id
-        }
-        const post = await Post.create(newPostData);
-        const user = await User.findById(req.user._id);
-        user.posts.push(post._id);
-        await user.save();
-        res.status(201).json({
-            success:true,
-            post
-        })
-        
-    } catch (err) {
-        res.status(500).json({
-            success:false,
-            message:err.message
-        })
-        
+exports.createPost = async (req, res) => {
+  try {
+    const newPostData = {
+      caption: req.body.caption,
+      image: {
+        //Todo
+        public_id: "req.body.public_id",
+        url: "req.body.url",
+      },
+      owner: req.user._id,
+    };
+    const post = await Post.create(newPostData);
+    const user = await User.findById(req.user._id);
+    user.posts.push(post._id);
+    await user.save();
+    res.status(201).json({
+      success: true,
+      post,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
+
+exports.deletePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post Not Found",
+      });
     }
-}
 
-exports.deletePost = async(req, res)=>{
-    try {
-        const post = await Post.findById(req.params.id);
-        
-        if(!post){
-            return res.status(404).json({
-                success:false,
-                message:"Post Not Found"
-            })
-        }
-
-        //user is not authorised
-        if(post.owner.toString() !== req.user._id.toString()){
-            return res.status(401).json({
-                success:false,
-                message:"Unauthorised"
-            })
-        }
-        
-        // Delete Post
-        await post.remove();
-
-        //remove post id from user model
-        const user = await User.findById(req.user._id);
-        user.posts.pull(req.params.id);
-        await user.save();
-        return res.status(200).json({
-            success:true,
-            message:"Post Deleted"
-        })
-        
-    } catch (error) {
-        res.status(500).json({
-            success:false,
-            message: error.message
-        })
-        
+    //user is not authorised
+    if (post.owner.toString() !== req.user._id.toString()) {
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorised",
+      });
     }
-}
 
+    // Delete Post
+    await post.remove();
+
+    //remove post id from user model
+    const user = await User.findById(req.user._id);
+    user.posts.pull(req.params.id);
+    await user.save();
+    return res.status(200).json({
+      success: true,
+      message: "Post Deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
 //create like and unlike controller
-exports.likeAndUnlikePost = async(req, res)=>{
-    
-    try {
-        const post = await Post.findById(req.params.id);
-    if(!post){
-        return res.status(404).json({
-            success:false,
-            message:"Post no found"
-        })
+exports.likeAndUnlikePost = async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(404).json({
+        success: false,
+        message: "Post no found",
+      });
     }
 
     //unlike condition
-    if(post.likes.includes(req.user._id)){
-        // const index = post.likes.indexOf(req.user._id);
+    if (post.likes.includes(req.user._id)) {
+      // const index = post.likes.indexOf(req.user._id);
 
-        // post.likes.splice(index, 1);
-        post.likes.pull(req.user._id)
+      // post.likes.splice(index, 1);
+      post.likes.pull(req.user._id);
 
-        await post.save();
-        
-        return res.status(200).json({
-            success:true,
-            message:"Post Unliked"
-        })
-    }else{
-        //created like
-    post.likes.push(req.user._id);
-    await post.save();
-    return res.status(200).json({
-        success:true,
-        message:"Post Liked"
-    })
+      await post.save();
 
+      return res.status(200).json({
+        success: true,
+        message: "Post Unliked",
+      });
+    } else {
+      //created like
+      post.likes.push(req.user._id);
+      await post.save();
+      return res.status(200).json({
+        success: true,
+        message: "Post Liked",
+      });
     }
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
 
-    
+//get post of following user
+exports.getPostFollowing = async (req, res) => {
+  console.log(req.user);
+  try {
+    const user = await User.findById(req.user._id);
+    const posts = await Post.find({
+      owner: {
+        $in: user.following,
+      },
+    });
+    return res.status(200).json({
+      success: true,
+      posts,
+    });
+  } catch (err) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};
 
+//update caption of post
+exports.updateCaption = async(req, res)=>{
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post){
+            return res.status(404).json({
+                success:false,
+                message:"Post not found"
+            })
+        }
+        if(post.owner.toString() !== req.user._id.toString()){
+            return res.status(401).json({
+                success:false,
+                message:"Unauthorised user"
+            })
+        }
+        post.caption = req.body.caption;
+        await post.save();
+        res.status(200).json({
+            success:true,
+            message:"Post Updated"
+        })
+        
     } catch (error) {
         res.status(500).json({
             success:false,
-            message: error.message
-        })
-    
-    }
-}
-
-//get post of following user
-exports.getPostFollowing = async(req, res)=>{
-    console.log(req.user)
-    try {
-        const user = await User.findById(req.user._id);
-        const posts = await Post.find({
-            owner:{
-                $in: user.following,
-            }
-        })
-        return res.status(200).json({
-            success:true,
-            posts,
-        })
-
-    } catch (err) {
-        res.status(500).json({
-            success:false,
-            message:err.message
+            message:error.message
         })
         
     }
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -12,21 +12,29 @@ router
     postController.createPost
   );
 
-router.route("/posts").get(passport.authenticate("jwt", {session:false}), postController.getPostFollowing);
+router
+  .route("/posts")
+  .get(
+    passport.authenticate("jwt", { session: false }),
+    postController.getPostFollowing
+  );
 
-//route to like and unlike post and delete post
+//route to like and unlike post and delete post and update the caption
 router
   .route("/:id")
   .get(
     passport.authenticate("jwt", { session: false }),
     postController.likeAndUnlikePost
+  ).put(
+    passport.authenticate("jwt", {session:false}),
+    postController.updateCaption
   )
   .delete(
     passport.authenticate("jwt", { session: false }),
     postController.deletePost
   );
 
-
-
+//update the caption
+// router.route("/update/caption/:id").put(passport.authenticate("jwt", {session:false}), postController.updateCaption);
 
 module.exports = router;
